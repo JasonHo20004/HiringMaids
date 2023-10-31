cdb0fe597356a3539ea49cc3869f96bf12310191
MSI\datho <dathodz2004@gmail.com>
2023-10-31 22:55:42 +0700

    rework filterDomesticHelper: + add filt by Address + add filt by SalaryOffer + add filt by WorkMode

--- ByApp.cs
+++ ByApp.cs
@@ -22,11 +22,11 @@ namespace ConsoleApp1
         public void createData()
         {
             listHelper = new List<DomesticHelper>();
-            DomesticHelper staff1 = new DomesticHelper("Dat", "0123", "Quan 1", new DateTime(2023, 1, 1), 100, new List<string> { "cooking", "cleaning" }, "DH01");
+            DomesticHelper staff1 = new DomesticHelper("Dat", "0123", "Quan 1", new DateTime(2023, 1, 1), 120, new List<string> { "cooking", "cleaning" }, "DH01", "Full-Time");
             listHelper.Add(staff1);
-            DomesticHelper staff2 = new DomesticHelper("Vu", "0123", "Quan 2", new DateTime(2023, 06, 24), 100, new List<string> { "cooking" }, "DH02");
+            DomesticHelper staff2 = new DomesticHelper("Vu", "0123", "Quan 2", new DateTime(2023, 06, 24), 150, new List<string> { "cooking" }, "DH02", "Part-time");
             listHelper.Add(staff2);
-            DomesticHelper staff3 = new DomesticHelper("Loc", "0123", "Thu Duc", new DateTime(2023, 1, 24), 100, new List<string> { "cooking", "taking care of child" }, "DH03");
+            DomesticHelper staff3 = new DomesticHelper("Loc", "0123", "Thu Duc", new DateTime(2023, 1, 24), 200, new List<string> { "cooking", "taking care of child" }, "DH03", "Full-Time");
             listHelper.Add(staff3);
         }
         public void printListDomesticHelper()
@@ -44,36 +44,52 @@ namespace ConsoleApp1
 
         public void filterDomesticHelper()
         {
-            // Bây giờ bổ sung thêm để có thể lọc theo các tiêu chí : mức lương, yêu cầu công việc, địa điểm, hình thức công việc part time hay full time
-            // Bổ sung thêm vào class DomesticHelper thuộc tính hình thức làm việc để có thể quản lý hình thức làm việc của helper
-            Console.WriteLine("Input offer salary: ");
-            inputSalaryOffer();
-            Console.WriteLine("Input the number of requirements: ");
-            int n = int.Parse(Console.ReadLine());
-            // Nhập và add vào wish list yêu cầu của người thuê
-            List<string> list = new List<string>();
-            for (int i = 0; i < n; i++)
+            bool continueFiltering = true;
+            while (continueFiltering)
             {
-                Console.WriteLine("Requirement " + (i + 1));
-                string s = Console.ReadLine();
-                s = s.ToLower();
-                list.Add(s);
-            }
-            // Lọc ra những staff thõa requirements 
-            var filteredStaff = listHelper.Where(helper =>helper.getSalaryOffer() <= employer.getSalaryOffer() 
-                                                &&list.All(req => helper.HasSkill(req)));
-
-            if (filteredStaff.Count() > 0)
-            {
-                Console.WriteLine("Filtered Domestic Helpers:");
-                foreach (var helper in filteredStaff)
+                Console.WriteLine("Input offer salary: ");
+                inputSalaryOffer();
+                Console.WriteLine("Input the number of requirements: ");
+                int n = int.Parse(Console.ReadLine());
+                List<string> list = new List<string>();
+                for (int i = 0; i < n; i++)
                 {
-                    helper.printInfo();
+                    Console.WriteLine("Requirement " + (i + 1));
+                    string s = Console.ReadLine();
+                    s = s.ToLower();
+                    list.Add(s);
+                }
+                Console.WriteLine("Input desired location: ");
+                string location = Console.ReadLine().ToLower();
+                Console.WriteLine("Input work mode (part-time/full-time): ");
+                string workMode = Console.ReadLine().ToLower();
+
+                var filteredStaff = listHelper.Where(helper => helper.getSalaryOffer() <= employer.getSalaryOffer()
+                                                    && list.All(req => helper.HasSkill(req))
+                                                    && helper.GetLocation().ToLower() == location
+                                                    && helper.GetWorkMode().ToLower() == workMode.ToLower());
+
+                if (filteredStaff.Count() > 0)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("======= Filtered Domestic Helpers ========");
+                    foreach (var helper in filteredStaff)
+                    {
+                        helper.printInfo();
+                    }
+                    continueFiltering = false;
+                }
+                else
+                {
+                    Console.WriteLine("No data suit your input. Do you want to input again? (Y/N)");
+                    string userInput = Console.ReadLine().ToUpper();
+                    if (userInput != "Y")
+                    {
+                        continueFiltering = false;
+                    }
                 }
             }
-            else { Console.WriteLine("Empty data !!!"); }
         }
-
 
     }
 }
--- DomesticHelper.cs
+++ DomesticHelper.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
     internal class DomesticHelper : Person
-    {   
+    {
         //private string[] Skills { get; set; }
         //private WorkHistory[] WorkHistory { get; set; }
         //private RatingHistory RatingHistory { get; set; }
@@ -20,33 +20,51 @@ namespace ConsoleApp1
         List<Contract> contracts;
         float salaryOffer;
         string id;
+        string workMode; // Thêm thuộc tính hình thức làm việc
+
         // Constructor
-        public DomesticHelper(string name, string phoneNumber, string address, DateTime dob, float salaryOffer,List<string>skills, string id) : base(name, phoneNumber, address, dob)
+        public DomesticHelper(string name, string phoneNumber, string address, DateTime dob, float salaryOffer, List<string> skills, string id, string workMode) : base(name, phoneNumber, address, dob)
         {
             this.salaryOffer = salaryOffer;
             this.skills = skills;
             this.id = id;
+            this.workMode = workMode; // Thiết lập giá trị cho thuộc tính hình thức làm việc
         }
+
         // Methods
         public float getSalaryOffer()
         {
             return salaryOffer;
         }
+
         public override string toString()
         {
             string id = "ID: " + this.id + "\t";
             string info = base.toString();
             info += $"Salary offer: {salaryOffer} \t";
             info += $"Skills: " + string.Join(", ", skills) + "\n";
+            info += $"Work mode: {workMode}"; //Hình thức làm việc.
             return id + info;
         }
+
         public override void printInfo()
         {
             base.printInfo();
         }
+
         public bool HasSkill(string skill)
         {
             return skills.Contains(skill);
         }
+        public string GetLocation()
+        {
+            return this.address;
+        }
+
+        public string GetWorkMode()
+        {
+            return this.workMode;
+        }
     }
+
 }
